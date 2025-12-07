e4a3f76b389e4e1ee037545f99cb5dbc0567300c
Shagor <infinityshagor18557@gmail.com>
2025-12-07 23:38:50 +0600

    firebase admin added

--- encode.js
+++ encode.js
@@ -1,7 +1,7 @@
 // encode.js
 const fs = require("fs");
 const key = fs.readFileSync(
-  "./final-project-zipshift-curier-firebase-admin.json",
+  "./ticket-time-dcb40-firebase-adminsdk.json",
   "utf8"
 );
 const base64 = Buffer.from(key).toString("base64");
